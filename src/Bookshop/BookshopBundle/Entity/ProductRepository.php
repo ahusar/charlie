493f72966561a1cf3e29506c3487c8d024a85bda
Andrei Husar <ahusar@pitechnologies.ro>
2013-10-21 12:00:39 +0300

    Refactoring Category Controller & Search Controller

--- src/Bookshop/BookshopBundle/Entity/ProductRepository.php
+++ src/Bookshop/BookshopBundle/Entity/ProductRepository.php
@@ -12,15 +12,6 @@ use Doctrine\ORM\EntityRepository;
  */
 class ProductRepository extends EntityRepository {
 
-    public function getProducts($categoryid) {
-        $qb = $this->createQueryBuilder('c')
-                ->select('c')->where('c.category = :categ_id AND c.active = 1')
-                ->setParameter('categ_id', $categoryid);
-
-        return $qb->getQuery()
-                        ->getResult();
-    }
-
     public function getLast($nr) {
         $qb = $this->createQueryBuilder('l')
                 ->select('l')->orderBy('l.id', 'desc')
@@ -37,7 +28,7 @@ class ProductRepository extends EntityRepository {
                 ->setParameter('product_id', $productid);
 
         return $qb->getQuery()
-                        ->getResult();
+                        ->getSingleResult();
     }
 
     public function relatedProducts($categoryid) {
@@ -49,29 +40,27 @@ class ProductRepository extends EntityRepository {
         return $qb->getQuery()
                         ->getResult();
     }
-    
-    public function isOnStock($id, $quantity)
-    {
-       $qb = $this->createQueryBuilder('s')
+
+    public function isOnStock($id, $quantity) {
+        $qb = $this->createQueryBuilder('s')
                 ->select('s.stock')->where('s.id = :id AND s.active = 1')
                 ->setParameter('id', $id);
-       $result=$qb->getQuery()->getSingleResult();
+        $result = $qb->getQuery()->getSingleResult();
 
-       if($result['stock'] >= $quantity)
-           return true;
-       else
-           return false;
+        if ($result['stock'] >= $quantity)
+            return true;
+        else
+            return false;
     }
-    
-    public function addStock($id, $quantity)
-    {
+
+    public function addStock($id, $quantity) {
         $qb = $this->createQueryBuilder('s')
                 ->select('s.stock')->where('s.id = :id AND s.active = 1')
                 ->setParameter('id', $id);
         $result = $qb->getQuery()->getSingleResult();
         $prevStock = $result['stock'];
-        
-        if($prevStock+$quantity >= 0){
+
+        if ($prevStock + $quantity >= 0) {
             $qb = $this->createQueryBuilder('s')
                     ->update()->set('s.stock', $prevStock + $quantity)
                     ->where('s.id= :id')
@@ -80,45 +69,88 @@ class ProductRepository extends EntityRepository {
         }
     }
 
-
-    public function getNrAllProducts($filter)
-    {
+    public function getNrAllProducts($filter) {
         $em = $this->getEntityManager();
         return $em->createQuery('SELECT COUNT(p) FROM BookshopBookshopBundle:Product p INNER JOIN BookshopBookshopBundle:Category c WITH c = p.category WHERE 1=1' . $filter)
-                ->getSingleScalarResult();
+                        ->getSingleScalarResult();
     }
-    public function getAllProductsQuery($request)
-    {
-        $filter = $this->createSqlFilter($request);
-        $count = $this->getNrAllProducts($filter);
+
+    public function getAllProductsQuery($filter, $count) {
         $em = $this->getEntityManager();
         $dql = "SELECT p FROM BookshopBookshopBundle:Product p INNER JOIN BookshopBookshopBundle:Category c WITH c = p.category WHERE 1=1";
         $dql.=$filter;
 
         return $em->createQuery($dql)->setHint('knp_paginator.count', $count);
-        
     }
-    
-    private function createSqlFilter($request){
-        $filter = "";
-        if (strlen($request->query->get('title'))>0) {
-            $filter.= " AND p.title like '%" . $request->query->get('title') . "%'";
-        }
-        if (strlen($request->query->get('category'))>0) {
-            $filter.= " AND c.id = " . $request->query->get('category');
+
+    public function getProduse($categoryid, $request) {
+        $filters = $this->createSqlFilter($request);
+        $em = $this->getEntityManager();
+
+        $qb = "SELECT a from BookshopBookshopBundle:Product a where a.category = " . $categoryid . " and a.active = 1 ";
+        $qb .= $filters;
+
+        return $em->createQuery($qb);
+    }
+
+    public function searchProduct($request) {
+        $em = $this->getEntityManager();
+        $filters = $this->createSqlFilter($request);
+
+        $qb = " SELECT a from BookshopBookshopBundle:Product a where 1=1 ";
+        $qb .= $filters;
+        return $em->createQuery($qb);
+    }
+
+    private function createSqlFilter($request) {
+
+        $filter = '';
+
+        if (strlen($request->get('q')) > 0) {
+            $filter .= "a.title like '%" . $request->get('q') . "%'";
         }
 
-        if (strlen($request->query->get('stock'))>0)
-            switch ($request->query->get('stock')) {
-                case 'on':
-                    $filter .= ' and p.stock>0';
+        if (strlen($request->get('stock')) > 0)
+            switch ($request->get('stock')) {
+                case '1':
+                    $filter .= ' and a.stock>0';
                     break;
-            } else {
-            if (strlen($request->query->get('title'))>0 || strlen($request->query->get('category'))>0) {
-                $filter .= ' and p.stock>=0';
+                case '0':
+                    $filter .= ' and a.stock=0';
+            }
+        if (strlen($request->get('pricerange')) > 0) {
+            switch ($request->get('pricerange')) {
+                case '1':
+                    $filter .= ' and a.price between 0 and 49.99';
+                    break;
+                case '2':
+                    $filter .= ' and a.price between 50 and 99.99';
+                    break;
+                case '3':
+                    $filter .= ' and a.price>100';
+                    break;
             }
         }
-        
+
+        if (strlen($request->get('category')) > 0) {
+            if (ctype_digit($request->get('category'))) {
+                $filter .= ' and a.category = ' . $request->get('category');
+            }
+        }
+
+//        if (strlen($request->get('sort')) > 0)
+//            if (($request->get('sort') != 'a.price') && ($request->get('sort') != 'a.title')) {
+//                $request->get('sort') = 'a.price';
+//            }
+//
+//        if (strlen($request->get('page')) > 0)
+//            if (!ctype_digit($request->get('page')))
+//                $request->get('page') = '1';
+//
+//        if (strlen($request->get('direction')) > 0)
+//            if (($request->get('direction') != 'asc') && ($request->get('direction') != 'desc'))
+//                $request->get('direction') = 'asc';
+
         return $filter;
     }
 
